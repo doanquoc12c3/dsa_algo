8d39eec35a75381fcd64688d0ee30fbae0fd8648
unknown <doanquoc12c3>
2023-08-25 16:35:24 +0800

    merge sorted array

--- src/array_string/Solution.java
+++ src/array_string/Solution.java
@@ -1,3 +1,5 @@
+package array_string;
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         //variables to work as pointers
--- src/array_string/Test.java
+++ src/array_string/Test.java
@@ -1,3 +1,5 @@
+package array_string;
+
 public class Test {
     public static void main(String[] args) {
         Solution s = new Solution();
